6106146af67c502892b86bcbeccd3211dd9c14b0
Enmanuel Chipana <emachipana0102@gmail.com>
2023-09-08 12:48:59 -0500

    SETUP: connection to database

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import plansRoutes from "./routes/plans.routes.js";
 import registersRoutes from "./routes/registers.routes.js";
 import usersRoutes from "./routes/users.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import morgan from "morgan";
 
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(morgan("dev"));
 
 // routes
 app.use("/", indexRoute);
