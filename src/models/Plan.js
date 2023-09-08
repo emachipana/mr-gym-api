47bdd8b0acbaf75affe0a776f0ed8062cdc85fa9
Enmanuel Chipana <emachipana0102@gmail.com>
2023-09-08 12:58:05 -0500

    ADD: plan model

---
+++ src/models/Plan.js
@@ -0,0 +1,36 @@
+import { Schema } from "mongoose";
+
+// schema
+const planSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Schema.Types.Decimal128,
+      required: true
+    },
+    benefits: {
+      type: String,
+      required: true
+    },
+    remaining: {
+      type: Number,
+      required: true
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+// model
+const Plan = mongoose.model("Plan", planSchema);
+
+export default Plan;
