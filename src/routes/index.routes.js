e5d354611d75d1feccdbb2742a7d1897edf646cb
Enmanuel Chipana <emachipana0102@gmail.com>
2023-09-29 10:37:43 -0500

    ADD: routes on main route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,7 +5,36 @@ const router = Router();
 // index route
 router.get("/", (_req, res) => {
   res.json({
-    message: "Bienvenido a la API del gimnasio Mr. Gym"
+    message: "Bienvenido al API del gimnasio Mr. Gym",
+    routes: {
+      Auth: {
+        SignIn: "/auth/signin",
+        SignUp: "/auth/signup"
+      },
+      Plan: {
+        GetPlans: "/plans",
+        GetPlanById: "/plans/:id",
+        CreatePlan: "/plans",
+        UpdatePlan: "/plans/:id",
+        DeletePlan: "/plans/:id"
+      },
+      Register: {
+        GetRegisters: "/registers",
+        GetRegisterById: "/registers/:id",
+        GetClientRegisters: "/registers/client/myRegisters",
+        CreateRegister: "/registers",
+        UpdateRegister: "/registers/:id",
+        DeleteRegister: "/registers/:id"
+      },
+      User: {
+        GetUsers: "/users",
+        GetUserById: "/users/:id",
+        GetClientProfile: "/users/info/profile",
+        CreateUser: "/users",
+        UpdateUser: "/users/:id",
+        DeleteUser: "/users/:id"
+      }
+    }
   });
 });
 
